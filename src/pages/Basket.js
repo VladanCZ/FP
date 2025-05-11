1dc4d4d0af431d27ca964598b3fd1cc7db2a00a2
your GitHub login <your email>
2025-05-11 19:12:51 +0200

    form

--- src/pages/Basket.js
+++ src/pages/Basket.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { useCart } from "../CartContext";
+import { useNavigate } from "react-router-dom";
 import "./Basket.scss";
 
 const Basket = () => {
   const { cart, dispatch } = useCart();
+  const navigate = useNavigate();
 
   const handleQuantityChange = (id, quantity) => {
     dispatch({ type: "UPDATE_QUANTITY", payload: { id, quantity: parseInt(quantity) } });
@@ -19,24 +21,32 @@ const Basket = () => {
       {cart.length === 0 ? (
         <p>Košík je prázdný.</p>
       ) : (
-        <ul>
-          {cart.map((item) => (
-            <li key={item.id}>
-              <img src={item.src} alt={item.alt} width="50" />
-              <span>{item.description}</span>
-              <input
-                type="number"
-                min="1"
-                value={item.quantity}
-                onChange={(e) => handleQuantityChange(item.id, e.target.value)}
-              />
-               <button onClick={() => handleRemove(item.id)}>Odstranit</button>
-            </li>
-          ))}
-        </ul>
+        <>
+          <ul>
+            {cart.map((item) => (
+              <li key={item.id}>
+                <img src={item.src} alt={item.alt} width="50" />
+                <span>{item.description}</span>
+                <input
+                  type="number"
+                  min="1"
+                  value={item.quantity}
+                  onChange={(e) => handleQuantityChange(item.id, e.target.value)}
+                />
+                <button onClick={() => handleRemove(item.id)}>Odstranit</button>
+              </li>
+            ))}
+          </ul>
+          <div className="basket-actions">
+            <button className="continue-btn" onClick={() => navigate("/Order")}>
+              Pokračovat
+            </button>
+          </div>
+        </>
       )}
     </div>
   );
 };
 
 export default Basket;
+
