37dc1e85cbf8b7fbf09fa9acce916bdf0cc19922
your GitHub login <your email>
2025-05-28 18:39:25 +0200

    basket

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { useCart } from "../../context/CartContext"; 
+import { useCart } from "../../context/CartContext";
 import "./Navigation.scss";
-import { FaShoppingCart } from "react-icons/fa"; 
+import { FaShoppingCart } from "react-icons/fa";
 
 const navItems = [
   { name: "Sesterna", path: "/Sesterna" },
@@ -16,6 +16,16 @@ const Navigation = () => {
   const { cart } = useCart();
   const totalItems = cart.reduce((sum, item) => sum + item.quantity, 0);
 
+  const [animate, setAnimate] = useState(false);
+
+  useEffect(() => {
+    if (totalItems > 0) {
+      setAnimate(true);
+      const timeout = setTimeout(() => setAnimate(false), 500); 
+      return () => clearTimeout(timeout);
+    }
+  }, [totalItems]);
+
   return (
     <nav className="nav-bar navigation">
       {navItems.map((item) => (
@@ -25,12 +35,17 @@ const Navigation = () => {
       ))}
       <Link to="/Kosik" className="nav-link cart-link">
         <FaShoppingCart size={20} />
-        {totalItems > 0 && <span className="cart-count">{totalItems}</span>}
+        {totalItems > 0 && (
+          <span
+            className={`cart-count ${animate ? "bounce" : ""}`}
+          >
+            {totalItems}
+          </span>
+        )}
       </Link>
     </nav>
   );
 };
 
 export default Navigation;
-
 
