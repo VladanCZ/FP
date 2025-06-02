b32fbae82c165c87d12e94b00234ddce4bbaa564
your GitHub login <your email>
2025-06-02 18:43:27 +0200

    Sister_hp

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Order from "./pages/Order";
 
 import Home from "./pages/Home";
@@ -15,6 +15,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}>
+        <Route index element={<Navigate to="/Sesterna" replace />} />
           <Route path="Sesterna" element={<Sister />} />
           <Route path="Pokoj-pacienta" element={<PatientsRoom />} />
           <Route path="Socialni-zarizeni" element={<SanitaryFacilities />} />
